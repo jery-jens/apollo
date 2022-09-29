8a368eddc3c116f090a48f72b769f93069e5bb03
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-09-29 14:20:25 +0200

    :rocket: Let's start!

---
+++ js/index.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Variables
+    const path = window.location.pathname ?? null;
+    const access = Boolean(sessionStorage.getItem("access_granted")) ?? false;
+
+    // Routes
+    const lockedRoute = window.location.origin + "/locked";
+    const baseRoute = window.location.origin;
+
+    (() => {
+        if (path === "/") {
+            return;
+        } else if (path === "/locked") {
+            access ? window.location.href === baseRoute : window.location.href === lockedRoute;
+        } else {
+            access ? window.location.href === lockedRoute : null;
+        };
+    })();
+});
