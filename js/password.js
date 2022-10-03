16710d31216f1d6b41f67cb1d97eee9170dd927f
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-10-03 13:14:58 +0200

    Update password.js

--- js/password.js
+++ js/password.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const passwordField = document.querySelector("#password");
 
     passwordField.addEventListener("onchange", (e) => {
-        
+        console.log(e);
     });
 });
