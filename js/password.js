037551db3484589bd4872d57a69bacb13b5fe226
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-10-03 13:17:52 +0200

    Update password.js

--- js/password.js
+++ js/password.js
@@ -1,7 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Elements
     const passwordField = document.querySelector("#password");
+    const submitButton = document.querySelector(".inner-submit");
+
+    // Variables
+    const password = "test";
 
     passwordField.addEventListener("onchange", (e) => {
         console.log(e);
+    });
+
+    Webflow.push(function() {
+        $('form').submit(function() {
+            console.log(passwordField.value);
+            return false;
+        });
     });
 });
