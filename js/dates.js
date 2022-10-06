5ebff11be17d66876f94d7faf9493bf974b1ae63
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-10-06 14:51:02 +0200

    Update dates.js

--- js/dates.js
+++ js/dates.js
@@ -12,8 +12,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const cTop = window.scrollY + checker.getBoundingClientRect().top;
 
     window.addEventListener("scroll", () => {
-        console.log(window.scrollY, wBottom, wrapper.getBoundingClientRect())
+        var rect = wrapper.getBoundingClientRect();
+
+        const inView =
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth);
         
+        console.log(inView)
         if (cTop ) {
             dates.forEach((date) => {
 
