4127454eb7ecd389beec72e47a90d66c17b0bd50
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-10-06 16:16:31 +0200

    Update dates.js

--- js/dates.js
+++ js/dates.js
@@ -13,39 +13,60 @@ document.addEventListener("DOMContentLoaded", () => {
         const offset = window.innerHeight + window.scrollY;
         const margin = offset - wTop;
         const downScroll = prevScroll < window.scrollY ? true : false;
-        console.log(margin,wrapper.getBoundingClientRect().top)
+        let order = [0, 1, 2, 3, 4, 5, 6];
 
         if (margin >= 0) {
             if (margin >= 0 && margin < 90) {
                 console.log("hide them all")
+                order = [0, 1, 2, 3, 4, 5, 6];
             };
 
             if (margin >= 90 && margin < 180) {
                 console.log("show 1");
+                order = [0, 1, 2, 3, 4, 5, 6];
             };
             
             if (margin >= 180 && margin < 270) {
                 console.log("show 2");
+                order = [1, 2, 3, 4, 5, 6, 0];
+
             };
 
             if (margin >= 270 && margin < 360) {
                 console.log("show 3");
+                order = [2, 3, 4, 5, 6, 0, 1];
             };
 
             if (margin >= 360 && margin < 450) {
                 console.log("show 4");
+                order = [3, 4, 5, 6, 0, 1, 2];
             };
 
             if (margin >= 450 && margin < 540) {
                 console.log("show 5");
+                order = [4, 5, 6, 0, 1, 2, 3];
             };
 
             if (margin >= 540 && margin < 630) {
                 console.log("show 6");
+                order = [5, 6, 0, 1, 2, 3, 4];
             };
 
             if (margin >= 630 && margin < 720) {
                 console.log("show 7");
+                order = [6, 0, 1, 2, 3, 4, 5];
+            };
+
+            for (let i = 0; i < dates.length; i++) {
+                const prevClassName = `date-item-${order[i-1]}`;
+                const nextClassName = `date-item-${order[i+1]}`;
+                const className = `date-item-${order[i]}`;
+
+                console.log("remove", prevClassName, nextClassName);
+                console.log("add", className);
+
+                dates[i].classList.remove(prevClassName, nextClassName);
+                dates[i].classList.add(className);
             };
         };
 
