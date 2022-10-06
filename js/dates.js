bb0ec4047aeb3f684767c33d2c7cf53d27162fe2
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-10-06 14:00:35 +0200

    Update dates.js

--- js/dates.js
+++ js/dates.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log('test');
+    // Elements
+    const checker = document.querySelector(".js-date-checker");
+    const wrapper = checker.parentElement;
+
+    // Positions
+    const wTop = wrapper.getBoundingClientRect().top;
+    console.log(wTop);
 });
