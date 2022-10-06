74c81dabeb7f44063081b3d5ffc6c8ac31afc474
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-10-06 15:58:42 +0200

    Update dates.js

--- js/dates.js
+++ js/dates.js
@@ -17,7 +17,35 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (margin >= 0) {
             if (margin >= 0 && margin < 90) {
+                console.log("hide them all")
+            };
 
+            if (margin >= 90 && margin < 180) {
+                console.log("show 1");
+            };
+            
+            if (margin >= 180 && margin < 270) {
+                console.log("show 2");
+            };
+
+            if (margin >= 270 && margin < 360) {
+                console.log("show 3");
+            };
+
+            if (margin >= 360 && margin < 450) {
+                console.log("show 4");
+            };
+
+            if (margin >= 450 && margin < 540) {
+                console.log("show 5");
+            };
+
+            if (margin >= 540 && margin < 630) {
+                console.log("show 6");
+            };
+
+            if (margin >= 630 && margin < 720) {
+                console.log("show 7");
             };
         };
 
