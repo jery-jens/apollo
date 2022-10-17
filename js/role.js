d92eb56d723a4bd4e6c159c4f0a3df2ce43233af
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-10-17 14:07:55 +0200

    Update role.js

--- js/role.js
+++ js/role.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const choices = document.querySelectorAll(".choice");
     const checkes = document.querySelectorAll(".select-choice-wrapper");
+    const submit = document.querySelector(".role-submit");
+    let role = "participant";
 
     choices.forEach((choice, index) => {
         choice.addEventListener("click", () => {
@@ -9,6 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     if (index === innerIndex) {
                         choices[innerIndex].classList.add("active");
                         checkes[innerIndex].classList.add("active");
+                        role = choices[innerIndex].id;
                     } else {
                         choices[innerIndex].classList.remove("active");
                         checkes[innerIndex].classList.remove("active");
@@ -16,5 +19,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 });
             };
         });
+    });
+
+    submit.addEventListener("click", () => {
+        localStorage.setItem("role", role);
+        window.location.href = window.location.origin + "/dashboard";
     });
 });
