b1aea495398acd4e15ee4a39f31ee536d79c34f9
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-10-17 11:18:10 +0200

    Update role.js

--- js/role.js
+++ js/role.js
@@ -2,10 +2,18 @@ document.addEventListener("DOMContentLoaded", () => {
     const choices = document.querySelectorAll(".choice");
 
     choices.forEach((choice, index) => {
+        const check = choice.querySelector(".select-choice-wrapper");
+
         choice.addEventListener("click", () => {
             if (!choice.classList.contains("active")) {
                 choices.forEach((innerChoice, innerIndex) => {
-                    
+                    if (index === innerIndex) {
+                        choice.classList.add("active");
+                        check.classList.add("active");
+                    } else {
+                        choice.classList.remove("active");
+                        check.classList.remove("active");
+                    };
                 });
             };
         });
