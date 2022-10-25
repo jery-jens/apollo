e014fa23f61442f45ae5de68fee6cc6f952d44a5
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-10-25 17:11:13 +0200

    Update password.js

--- js/password.js
+++ js/password.js
@@ -1,40 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Elements
-    const passwordField = document.querySelector("#password");
-    const submitButton = document.querySelector(".submit");
-    const errorMessage = document.querySelector(".wrong-password");
+    let username = prompt("Please enter the username.");
+    while(username !== "test"){
+        username = prompt("Please enter the username."); 
+    };
 
-    window.addEventListener('keydown',function(e){if(e.keyIdentifier=='U+000A'||e.keyIdentifier=='Enter'||e.keyCode==13){if(e.target.nodeName=='INPUT'&&e.target.type=='text'){e.preventDefault();return false;}}},true);
-
-    // Variables
-    const password = "Tellit";
-    const scPassword = "tellit";
-
-    Webflow.push(function() {
-        $('form').submit(function() {
-            if (passwordField.value === password) {
-                sessionStorage.setItem("access_granted", true);
-                window.location.href = window.location.origin + "/home";
-            } else if (passwordField.value === scPassword) {
-                sessionStorage.setItem("access_granted", true);
-                window.location.href = window.location.origin + "/home";
-            } else {
-                errorMessage.style.display = "block";
-            };
-
-            document.querySelector("form").style.display = "flex";
-        });
-      });
-
-    submitButton.addEventListener("click", () => {
-        if (passwordField.value === password) {
-            sessionStorage.setItem("access_granted", true);
-            window.location.href = window.location.origin + "/home";
-        } else if (passwordField.value === scPassword) {
-            sessionStorage.setItem("access_granted", true);
-            window.location.href = window.location.origin + "/home";
-        } else {
-            errorMessage.style.display = "block";
-        };
-    });
+    let password = prompt("Please enter the password.");
+    while(password !== "test"){
+        password = prompt("Please enter the password."); 
+    };
 });
