0b60d994c3615ded5385aadf25cdc395d9b19386
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-12-07 14:39:29 +0100

    Update 🇪🇸

--- js/dashboard.js
+++ js/dashboard.js
@@ -34,5 +34,17 @@ document.addEventListener("DOMContentLoaded", () => {
      */
 
     const role = localStorage.getItem("role");
-    console.log(role);
+    const roleItems = document.querySelectorAll(".role");
+
+    roleItems.forEach((item, i) => {
+        if (role === "participant") {
+            roleItems[0].style.display = "flex";
+        } else if (role === "track-lead") {
+            roleItems[1].style.display = "flex";
+        } else if (role === "challenge-owner") {
+            roleItems[2].style.display = "flex";
+        } else {
+            item.style.display = "none";
+        };
+    });
 });
