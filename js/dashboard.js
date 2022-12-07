607d533869f0557f96f9ba59d021fb7aa5d5a10c
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-12-07 12:15:07 +0100

    Create dashboard 🇪🇸

---
+++ js/dashboard.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const dashItems = document.querySelectorAll(".dashboard-item");
+    
+    dashItems.forEach((item, i) => {
+        const btn = item.querySelector(".dashboard-item-btn");
+        const plus = btn.querySelector(".plus");
+        const minus = btn.querySelector(".minus");
+        const content = item.querySelector(".item-text");
+
+        btn.addEventListener("click", () => {
+            if (btn.classList.contains("active")) {
+                item.classList.remove("active");
+                btn.classList.remove("active");
+                content.classList.remove("active");
+
+                plus.style.display = "block";
+                minus.style.display = "none";
+            } else {
+                item.classList.add("active");
+                btn.classList.add("active");
+                content.classList.add("active");
+
+                plus.style.display = "none";
+                minus.style.display = "block";
+            };
+        });
+    });
+});
