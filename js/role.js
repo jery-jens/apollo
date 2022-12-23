54b7e7ab1c336065c01490d3e7f7a1a90d33a0fa
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-12-23 12:11:42 +0100

    Update role.js

--- js/role.js
+++ js/role.js
@@ -1,28 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const choices = document.querySelectorAll(".choice");
-    const checkes = document.querySelectorAll(".select-choice-wrapper");
-    const submit = document.querySelector(".role-submit");
-    let role = "participant";
+    const btns = document.querySelectorAll(".gotodashboard");
+    let target = "participant";
 
-    choices.forEach((choice, index) => {
-        choice.addEventListener("click", () => {
-            if (!choice.classList.contains("active")) {
-                choices.forEach((innerChoice, innerIndex) => {
-                    if (index === innerIndex) {
-                        choices[innerIndex].classList.add("active");
-                        checkes[innerIndex].classList.add("active");
-                        role = choices[innerIndex].id;
-                    } else {
-                        choices[innerIndex].classList.remove("active");
-                        checkes[innerIndex].classList.remove("active");
-                    };
-                });
+    btns.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            if (btn.classList.contains("participant")) {
+                target = "participant";
             };
-        });
-    });
 
-    submit.addEventListener("click", () => {
-        localStorage.setItem("role", role);
-        window.location.href = window.location.origin + "/dashboard";
+            if (btn.classList.contains("track-lead")) {
+                target = "track-lead";
+            };
+
+            if (btn.classList.contains("challenge-owner")) {
+                target = "challenge-owner";
+            };
+    
+            sessionStorage.setItem("target", target);
+        });
     });
 });
